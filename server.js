9dcd6907801c9fc0848c95450cf6dc9105746f02
Pedro Revilla <peterevilla@gmail.com>
2020-06-05 15:55:06 -0500

    mvp

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+const cors = require("cors");
+const restricted = require('./auth/restricted-middleware.js');
+const usersRouter = require("./users/users-router.js");
+const authRouter = require("./auth/auth-router");
 const server = express();
-server.use(express.json());
 
 
-server.get('/', (req, res) => {
-    res.send("<h2>Welcome to API</h2>")
-})
 
+const sessionConfig = {
+    name: 'sksession',
+    secret: 'myspeshulsecret',
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false, 
+      httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+  
+    store: new knexSessionStore(
+      {
+        knex: require("./data/dbConfig"),
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+      }
+    )
+  }
+  
+  server.use(express.json());
+  server.use(cors());
+  
+  server.use(session(sessionConfig));
+  
 
-module.exports = server;
+  server.use("/api/users", restricted, usersRouter);
+  server.use("/api/auth", authRouter);
+  
+  server.get("/", (req, res) => {
+    res.json({ api: "up" });
+  });
+  
+  module.exports = server;
